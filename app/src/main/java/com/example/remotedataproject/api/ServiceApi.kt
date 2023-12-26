95e4c263198e786651dd5d311be225b74aa5922c
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-12-26 17:46:49 +0100

    get one post added

--- app/src/main/java/com/example/remotedataproject/api/ServiceApi.kt
+++ app/src/main/java/com/example/remotedataproject/api/ServiceApi.kt
@@ -3,9 +3,13 @@ package com.example.remotedataproject.api
 import com.example.remotedataproject.model.Post
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ServiceApi {
 
     @GET("posts")
     fun getAllPosts() : Call<List<Post>>
+
+    @GET("posts/{id}")
+    fun getOnePost(@Path("id") ourId : Int) : Call<Post>
 }
--- app/src/main/java/com/example/remotedataproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/remotedataproject/ui/home/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.example.remotedataproject.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.remotedataproject.adapter.PostAdapter
 import com.example.remotedataproject.databinding.FragmentHomeBinding
@@ -17,6 +17,7 @@ import retrofit2.Response
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var listOfPosts : List<Post>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,25 +30,67 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onClick()
+    }
+
+    private fun onClick() {
         binding.btnCallApi.setOnClickListener {
+            getAllPosts()
+        }
+        binding.lvPost.setOnItemClickListener { parent, view, position, id ->
+            getOnePost(position)
+        }
+    }
 
-            provideRetrofitInstance().getAllPosts().enqueue(object : Callback<List<Post>?> {
-                override fun onResponse(call: Call<List<Post>?>, response: Response<List<Post>?>) {
-                    Log.d("TAG", "response status code ${response.code()}: ")
-                    Log.d("TAG", "response body ${response.body()}: ")
-
-                    val listOfPosts = response.body()
-                    if (listOfPosts != null) {
+    private fun getAllPosts() {
+        provideRetrofitInstance().getAllPosts().enqueue(object : Callback<List<Post>?> {
+            override fun onResponse(call: Call<List<Post>?>, response: Response<List<Post>?>) {
+                if (response.isSuccessful && response.body() != null) {
+                        listOfPosts = response.body()!!
                         val postAdapter = PostAdapter(requireContext(), listOfPosts)
                         binding.lvPost.adapter = postAdapter
-                    }
 
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Response is not successful",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
+            }
 
-                override fun onFailure(call: Call<List<Post>?>, t: Throwable) {
-                    Log.d("TAG", "error: ${t.message}")
+            override fun onFailure(call: Call<List<Post>?>, t: Throwable) {
+                Toast.makeText(
+                    requireContext(),
+                    "Failure due to internet connection / server timeout / etc",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+    }
+
+    private fun getOnePost(position : Int) {
+        val currentPost = listOfPosts[position]
+        provideRetrofitInstance().getOnePost(currentPost.id).enqueue(object : Callback<Post?> {
+            override fun onResponse(call: Call<Post?>, response: Response<Post?>) {
+                if (response.isSuccessful && response.body() != null) {
+                    Toast.makeText(requireContext(),"${response.body()?.body}",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Response is not successful",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
-            })
-        }
+            }
+
+            override fun onFailure(call: Call<Post?>, t: Throwable) {
+                Toast.makeText(
+                    requireContext(),
+                    "Failure due to internet connection / server timeout / etc",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
     }
 }
